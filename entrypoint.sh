2ee4169c7139668fb294854d3c64e4eff04bbdd9
tnair-c2s <88670474+tnair-c2s@users.noreply.github.com>
2021-09-08 12:41:17 -0400

    Feature/line length (#1)
    * add line length as option
    * fix indentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 ACCESS_TOKEN=$1
 SOURCE_PATH=$2
+LINE_LENGTH=$3
 
-black --diff $SOURCE_PATH > $HOME/black.patch
+black --diff $SOURCE_PATH --line-length $LINE_LENGTH > $HOME/black.patch
 python /apply_patches_as_suggestion.py --access-token $ACCESS_TOKEN $HOME/black.patch
